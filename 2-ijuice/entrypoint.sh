dc00802a026f8fdc3eae07a5aed4e24ad42f587e
MyloHDEX <mylo@hybriddex.com>
2020-08-20 09:44:19 +0000

    workaround rpcport required in conf file for chain

--- 2-ijuice/entrypoint.sh
+++ 2-ijuice/entrypoint.sh
@@ -3,6 +3,7 @@ mkdir -p /root/.komodo/IJUICE
 echo '#generated file from entrypoint.sh
 rpcuser=changeuser
 rpcpassword=changepassword
+rpcport=24708
 rpcallowip=127.0.0.1
 rpcallowip=172.17.0.1
 timeout=18000
