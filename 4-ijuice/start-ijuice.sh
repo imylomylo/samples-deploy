26807f9516766592a9be20703e05ff426d25ff19
MyloHDEX <mylo@hybriddex.com>
2020-08-20 15:21:17 +0000

    add scenario bc+spv+mm2+nodejs+mariadb

---
+++ 4-ijuice/start-ijuice.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/root/komodo/src/komodod -ac_name=IJUICE -ac_supply=1000 -ac_cc=2 -ac_pubkey=038afbc819aecfb3d65acdf270f755c9279d69d4a27293f10889264b8bd6c1fae2 -ac_reward=10000 -addnode=144.76.148.154 
+
