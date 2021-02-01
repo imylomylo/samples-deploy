1bd9c471a88f72a35b7eab07a5ae66813560649f
MyloHDEX <mylo@hybriddex.com>
2021-02-01 17:13:18 +0000

    blocknotify for jamjuice main node rm from dc

---
+++ simple-working/getinfo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+rpcuser=changeme
+rpcpassword=alsochangeme
+rpcport=64418
+curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"getinfo\", \"params\": []}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/
