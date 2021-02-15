14661003273d79785fb88513a1fa6df922af07b5
MyloHDEX <mylo@hybriddex.com>
2021-02-15 08:46:34 +0000

    update fund script for new chain name

--- simple-working/fund.list.sh
+++ simple-working/fund.list.sh
@@ -2,10 +2,11 @@
 SENDMANYARG="{"
 for address in `cat list.json | jq '.[][3]'`
 do
-   SENDMANYARG=${SENDMANYARG}${address}": 2.5," 
+   SENDMANYARG=${SENDMANYARG}${address}": 150.5," 
 done
 
 SENDMANYARG=`echo $SENDMANYARG | sed 's/.$//'`
 SENDMANYARG=$SENDMANYARG"}"
 
-echo "komodo-cli -ac_name=JAMJUICE sendmany \"\" '$SENDMANYARG'"
+echo "komodo-cli -ac_name=JAMJUICE2 sendmany \"\" '$SENDMANYARG'"
+echo "komodo-cli -ac_name=JAMJUICE2KV sendmany \"\" '$SENDMANYARG'"
