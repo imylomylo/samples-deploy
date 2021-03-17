e6918896f7a55f20f230bf0b88e78c0226822294
MyloHDEX <mylo@hybriddex.com>
2021-03-17 11:38:53 +0000

    backup .env file when upgrade jamjuice

--- simple-working/api-provider.jamjuice.upgrade.sh
+++ simple-working/api-provider.jamjuice.upgrade.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 CURRENT_VERSION=3
+cp .env .env.bak
 cp .env.api-provider.jamjuice${CURRENT_VERSION} .env
 /bin/rm -Rf ./api-provider/jamjuice-komodo-node/data
 /bin/rm -Rf ./api-provider/jamjuicekv-komodo-node/data
