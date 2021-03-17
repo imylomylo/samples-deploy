1ebf507b02a75f8dfb2bba1cd0dcdf5c8f0a2b25
MyloHDEX <mylo@hybriddex.com>
2021-03-17 10:51:06 +0000

    add some scripts for api-providers

---
+++ simple-working/api-provider/scripts/importprivkey.jamjuice.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#rpcuser=changeme
+#rpcpassword=alsochangeme
+#rpcport=33063
+source .env
+rpcuser=$KOMODO_SMARTCHAIN_NODE_USERNAME
+rpcpassword=$KOMODO_SMARTCHAIN_NODE_PASSWORD
+rpcport=$KOMODO_SMARTCHAIN_NODE_RPC_PORT
+wif=$THIS_NODE_WIF
+echo $rpcuser $rpcpassword @ $rpcport $raddress
+curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"importprivkey\", \"params\": [\"$wif\"]}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/
