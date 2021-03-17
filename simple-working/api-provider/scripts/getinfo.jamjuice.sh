33a810fb22917a09295344ca3e2ec0f9ac40ffee
MyloHDEX <mylo@hybriddex.com>
2021-03-17 11:05:19 +0000

    comment out echo for better jq use

--- simple-working/api-provider/scripts/getinfo.jamjuice.sh
+++ simple-working/api-provider/scripts/getinfo.jamjuice.sh
@@ -6,5 +6,5 @@ source ../../.env
 rpcuser=$KOMODO_SMARTCHAIN_NODE_USERNAME
 rpcpassword=$KOMODO_SMARTCHAIN_NODE_PASSWORD
 rpcport=$KOMODO_SMARTCHAIN_NODE_RPC_PORT
-echo $rpcuser $rpcpassword @ $rpcport
+#echo $rpcuser $rpcpassword @ $rpcport
 curl -s --user $rpcuser:$rpcpassword --data-binary "{\"jsonrpc\": \"1.0\", \"id\": \"curltest\", \"method\": \"getinfo\", \"params\": []}" -H 'content-type: text/plain;' http://127.0.0.1:$rpcport/
